fc7c1ef83d37779f9fa1b82c90d1396d2948df15
Robert Breunung <rbreunung@gmail.com>
2023-08-20 14:09:28 +0200

    working not perfect

--- postgresql/init/init-sonarqube-db.sql
+++ postgresql/init/init-sonarqube-db.sql
@@ -2,3 +2,4 @@ CREATE USER sonarqube;
 ALTER USER sonarqube WITH PASSWORD 'sonarqube-password';
 CREATE DATABASE sonarqube;
 GRANT ALL PRIVILEGES ON DATABASE sonarqube TO sonarqube;
+GRANT ALL PRIVILEGES ON SCHEMA public TO sonarqube;
